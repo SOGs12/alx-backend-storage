426a830e39623bf69f29fe544f7ae657cb823129
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-14 01:26:15 +0000

    0x01-NoSQL

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,23 @@
-#!/usr/bin/env python
-"""Script to generate stats about Nginx logs stored in MongoDB"""
+#!/usr/bin/env python3
+"""a Python script that provides some
+stats about Nginx logs stored in MongoDB"""
+
 
 from pymongo import MongoClient
 
-def get_log_stats():
-    # Connect to MongoDB
-    client = MongoClient()
-    db = client.logs
-    collection = db.nginx
-
-    # Get total number of logs
-    total_logs = collection.count_documents({})
-
-    # Get count of each method
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents({"method": method}) for method in methods}
-
-    # Get count of status check with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
-
-    # Display statistics
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
 
 if __name__ == "__main__":
-    get_log_stats()
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+
+    all_logs = collection.count_documents({})
+    get_logs = collection.count_documents({"method": "GET"})
+    post_logs = collection.count_documents({"method": "POST"})
+    put_logs = collection.count_documents({"method": "PUT"})
+    patch_logs = collection.count_documents({"method": "PATCH"})
+    delete_logs = collection.count_documents({"method": "DELETE"})
+    checks = collection.count_documents({"method": "GET", "path": "/status"})
+
+    print("{} logs\n Methods:\n \t  method GET: {} \n\t  method POST: {} \n\t  method"
+              " PUT: {} \n\t  method PATCH: {} \n\t  method DELETE: {}"
+              "\n status_checks: {}".format(all_logs, get_logs, post_logs, put_logs, patch_logs, delete_logs, checks))
