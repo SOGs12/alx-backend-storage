a413abf661a26c9dcbcb746c45961e68116dcfaa
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-15 19:24:05 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,6 +6,7 @@ from typing import Callable
 from functools import wraps
 import redis
 import requests
+
 redis_client = redis.Redis()
 
 
@@ -15,11 +16,13 @@ def url_count(method: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         url = args[0]
         redis_client.incr(f"count:{url}")
-        cached = redis_client.get(f'{url}')
+        cached = redis_client.get(url)
         if cached:
             return cached.decode('utf-8')
-        redis_client.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
+        else:
+            response = method(*args, **kwargs)
+            redis_client.setex(url, 10, response)
+            return response
     return wrapper
 
 
@@ -31,4 +34,4 @@ def get_page(url: str) -> str:
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    print(get_page('http://slowwly.robertomurray.co.uk'))
