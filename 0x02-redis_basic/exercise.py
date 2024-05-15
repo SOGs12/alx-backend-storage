e028eb606af55aa40d2a3595464175cc9381f89b
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-15 18:59:01 +0000

    redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python3
-"""A script to  Writing strings to Redis"""
-
+"""A script to Writing strings to Redis"""
 
 import redis
 import uuid
 from functools import wraps
-from typing import Union, Callable, Optional
-
-
+from typing import Union, Callable
 
 def count_calls(method: Callable) -> Callable:
-    """decorator takes a single method Callable
-    argument and returns a Callable"""
+    """Decorator to count method calls"""
+
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        # get the function name from the wrapped function
+        """Wrapper function for counting method calls"""
         self._redis.incr(key)
-
         return method(self, *args, **kwargs)
+
     return wrapper
 
-
-
 def call_history(method: Callable) -> Callable:
-    """decorator to store the history of
-    inputs and outputs for a particular function."""
-
+    """Decorator to store the history of inputs and outputs for a particular function"""
 
     key = method.__qualname__
     key_inputs = key + ":inputs"
@@ -35,16 +28,13 @@ def call_history(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """returns original method"""
-
+        """Wrapper function for storing input and output history"""
         res = method(self, *args, **kwargs)
-        self._redis.lpush(key_inputs, res)
-        self._redis.rpush(key_outputs, str(args))
-
+        self._redis.lpush(key_inputs, str(args))
+        self._redis.rpush(key_outputs, str(res))
         return res
 
     return wrapper
-
 
 class Cache:
     """A Cache class that stores data in Redis."""
@@ -56,49 +46,32 @@ class Cache:
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """method that takes a data argument and returns a string"""
+        """Store data in Redis and return the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-
-    def get(self, key: str,
-            fn: Callable = None) -> Union[str,
-                                                    bytes, int, float]:
-        """convert the data back to the desired format"""
-
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Retrieve data from Redis and apply optional conversion function"""
         data = self._redis.get(key)
         if fn:
             return fn(data) if data else None
         else:
             return data
 
-    def get_str(self, key: str):
-        """automatically parametrize Cache.get
-        with the correct conversion function"""
-        return self._redis.get(key, lambda x: x.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        """Retrieve data from Redis as string"""
+        return self.get(key, lambda x: x.decode("utf-8"))
 
-
-    def get_int(self, key: str):
-        """automatically parametrize Cache.get
-        with the correct conversion function"""
+    def get_int(self, key: str) -> int:
+        """Retrieve data from Redis as integer"""
         return self.get(key, lambda x: int(x))
 
-
-    def replay(self, method):
-        """
-        display the history of calls of a particular function
-
-        """
+    def replay(self, method: Callable):
+        """Display the history of calls of a particular function"""
         key = method.__qualname__
-        key_inputs= key + ":inputs"
-        key_outputs= key + ":outputs"
-        # The count_calls decorator, when applied to
-        # the store method, uses a separate key
-        # derived from the method's qualified name
-        # ("Cache.store" in this case) to keep
-        # track of how many times the store method has been called.
-        # This is a different key from the UUIDs used for storing data.
+        key_inputs = key + ":inputs"
+        key_outputs = key + ":outputs"
         count = self.get(key).decode("utf-8")
         print(f"Cache.{key} was called {count} times:")
         inputs = self._redis.lrange(key_inputs, 0, -1)
@@ -107,3 +80,10 @@ class Cache:
 
         for i, (input, output) in enumerate(zipped_list):
             print(f"{key} (*{output}) -> {input}")
+
+if __name__ == "__main__":
+    cache = Cache()
+    cache.store("first")
+    cache.store("second")
+    cache.store("third")
+    cache.replay(cache.store)
